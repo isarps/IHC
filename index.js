f8d8ac3e59b526c9b2dce0ec698262e4a999cff7
Isa <49652498+isarps@users.noreply.github.com>
2020-06-02 21:46:30 -0300

    inserindo furadeira e porta e tentando movimentar

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$ = (queryString) => document.querySelector(queryString);
+/*$ = (queryString) => document.querySelector(queryString);
   
 const shiftHue = (hue) => (hue + 1) % 360;
 
@@ -15,4 +15,27 @@ const animate = () => {
 };
 
 
-requestAnimationFrame(animate);
+requestAnimationFrame(animate);*/
+
+window.onload = () => {
+  if (AFRAME.utils.device.isMobile() == false) { // DESKTOP
+        var el = document.getElementById("mycursor");
+        el.setAttribute('cursor', 'rayOrigin: mouse;fuse: false');
+    } else {
+        var el = document.getElementById("mycursor"); // MOBILE
+        el.setAttribute('cursor', 'rayOrigin: cursor;fuse: true');
+        el.object3D.visible = true;
+    }
+}
+
+const porta = document.querySelector("porta");
+const seta1 = document.querySelector("setaDireita");
+const seta2 = document.querySelector("setaDireita0");
+
+function andarPraDireita() {
+  porta.object3D.rotation.y += 0.1;
+}
+
+seta1.addEventListener("click", andarPraDireita);
+seta2.addEventListener("click", andarPraDireita);
+
