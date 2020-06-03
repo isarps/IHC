080e87b57c1eaf5935c914af427ff0d706f7338f
angelovlima <angelovictor2001@hotmail.com>
2020-06-02 23:00:49 -0300

    Botao funcionando

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-/*$ = (queryString) => document.querySelector(queryString);
-  
-const shiftHue = (hue) => (hue + 1) % 360;
-
-let hue = 0;
-var button = 1;
-const animate = () => {
-    hue = shiftHue(hue);
-    const color = `hsl(${shiftHue(hue)}, 100%, 50%)`;
-    $('a-sphere').setAttribute('color', color);
-
-    const position = `0 ${1.5 + Math.sin(Date.now() / 1000)} -2`;
-    $('a-sphere').setAttribute('position', position);
-    requestAnimationFrame(animate);
-};
-
-
-requestAnimationFrame(animate);*/
-
 window.onload = () => {
   if (AFRAME.utils.device.isMobile() == false) { // DESKTOP
         var el = document.getElementById("mycursor");
@@ -28,14 +9,23 @@ window.onload = () => {
     }
 }
 
-const porta = document.querySelector("porta");
-const seta1 = document.querySelector("setaDireita");
-const seta2 = document.querySelector("setaDireita0");
+const botao = document.getElementById("botaoProximo");
+const furadeira = document.getElementById("furadeira");
 
-function andarPraDireita() {
-  porta.object3D.rotation.y += 0.1;
+function clickBotao(){
+  furadeira.object3D.rotation.y += 1;
 }
 
-seta1.addEventListener("click", andarPraDireita);
-seta2.addEventListener("click", andarPraDireita);
+botao.addEventListener("click", clickBotao);
+
+//const porta = document.querySelector("porta");
+//const seta1 = document.querySelector("setaDireita");
+//const seta2 = document.querySelector("setaDireita0");
+
+//function andarPraDireita() {
+  //porta.object3D.rotation.y += 0.1;
+//}
+
+//seta1.addEventListener("click", andarPraDireita);
+//seta2.addEventListener("click", andarPraDireita);
 
